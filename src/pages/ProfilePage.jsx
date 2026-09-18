f366699a50bb35b3676d68313de225a96a688c59
MalloryAnn <msoro02@jaguar.tamu.edu>
2026-09-18 09:17:40 -0500

    SCRUM-26 add initial profile page

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,32 @@
+function ProfilePage() {
+    // temporary filler
+  return (
+    <main>
+      <h1>User Profile</h1>
+
+      <p>
+        <strong>Display Name:</strong> Mallory Sorola
+      </p>
+
+      <p>
+        <strong>Username:</strong> mallorysorola
+      </p>
+
+      <p>
+        <strong>Bio:</strong> Computer Science student
+      </p>
+
+      <p>
+        <strong>Location:</strong> San Antonio, TX
+      </p>
+
+      <h2>Skills</h2>
+      <ul>
+        <li>Web Development</li>
+        <li>Python</li>
+      </ul>
+    </main>
+  )
+}
+
+export default ProfilePage
